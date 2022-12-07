b8abfed8d78b1a5c796299b0dbd579deee77549f
milan <milan@igrant.io>
2022-12-08 04:46:40 +0530

    DataWalletRecord

---
+++ data_wallet/src/main/java/io/igrant/data_wallet/utils/DataWalletRecord.kt
@@ -0,0 +1,42 @@
+package io.igrant.data_wallet.utils
+
+object DataWalletRecord {
+
+    fun add(
+        walletName:String,
+        id:String,
+        value:String,
+        tags:String
+    ){
+
+    }
+
+    fun update(
+        walletName:String,
+        id:String,
+        value:String
+    ){
+
+    }
+
+    fun delete(
+        walletName:String,
+        id:String
+    ){
+
+    }
+
+    fun get(
+        walletName:String,
+        id:String
+    ){
+
+    }
+
+    fun search(
+        walletName:String,
+        query:String
+    ){
+
+    }
+}
